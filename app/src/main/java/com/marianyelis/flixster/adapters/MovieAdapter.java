0d5d882fb4b79e71d9b4d44d9120867693bb3eec
PinkSylvie <marianyelis.jimenez1@upr.edu>
2022-02-11 19:40:22 -0400

    FLixster Part 2

--- app/src/main/java/com/marianyelis/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/marianyelis/flixster/adapters/MovieAdapter.java
@@ -1,19 +1,25 @@
 package com.marianyelis.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.marianyelis.flixster.DetailActivity;
 import com.marianyelis.flixster.R;
 import com.marianyelis.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,6 +59,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -62,6 +69,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
         }
 
@@ -69,6 +77,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            // Register click listener to a whole row
+            // Action taken to navigate to a new activity
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/marianyelis/flixster/models/Movie.java
+++ app/src/main/java/com/marianyelis/flixster/models/Movie.java
@@ -3,20 +3,29 @@ package com.marianyelis.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by parceler
+    public Movie() { }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fomJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -32,10 +41,20 @@ public class Movie {
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public String getOverview() {
+
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
